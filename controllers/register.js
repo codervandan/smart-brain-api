5c02cab87fd742ab1297a09b9ab414aa78a3200a
codervandan <devopdan@protonmail.com>
2025-09-24 14:50:35 -0700

    applied fixes to the register.js

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,8 @@
 // register.js
+
 const registerHandler = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+
   if (!email || !name || !password) {
     return res.status(400).json({ error: 'incorrect form submission' });
   }
@@ -15,17 +17,26 @@ const registerHandler = (req, res, db, bcrypt) => {
         const plainEmail = typeof loginEmail[0] === 'object' ? loginEmail[0].email : loginEmail[0];
         return trx('users')
           .insert({
-            name: name,
+            name,
             email: plainEmail,
             joined: new Date()
           })
           .returning('*')
           .then(userRows => res.json(userRows[0]))
-          .catch(() => res.status(400).json({ error: 'unable to register user' }));
+          .catch(err => {
+            console.error('Error inserting user:', err);
+            res.status(400).json({ error: 'unable to register user' });
+          });
       })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(() => res.status(400).json({ error: 'unable to register' }));
+      .then(() => trx.commit())
+      .catch(err => {
+        console.error('Transaction error during commit:', err);
+        trx.rollback();
+      });
+  }).catch(err => {
+    console.error('Transaction error:', err);
+    res.status(400).json({ error: 'unable to register' });
+  });
 };
 
 export default registerHandler;
