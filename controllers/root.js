110b4fc40d0b1c48dc48615aadd64ed34e169e22
codervandan <devopdan@protonmail.com>
2025-09-24 17:10:12 -0700

    new changes

--- controllers/root.js
+++ controllers/root.js
@@ -1,8 +1,21 @@
+// controllers/root.js
+
 const rootHandler = (req, res, db) => {
+  console.log("Root route hit");
+
   db.select('*')
     .from('users')
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json({ error: 'unable to fetch users' }));
+    .then(users => {
+      console.log("Fetched users:", users);
+      res.json(users);
+    })
+    .catch(err => {
+      console.error("Error fetching users:", err);
+      res.status(400).json({
+        error: 'unable to fetch users',
+        details: err.message
+      });
+    });
 };
 
 export default rootHandler;
